9a9c7708862d410c1afc26b9ba8e535399eeca34
palignusw <firstnamelastname252003@gmail.com>
2025-02-23 17:44:41 +0400

    npm run build

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,30 @@
-const withTM = require("next-transpile-modules")([
-  "antd",
-  "@ant-design/icons",
-  "@ant-design/icons-svg",
-  "rc-util",
-  "rc-pagination",
-  "rc-picker",
-  "rc-menu",
-  "rc-dropdown",
-  "rc-select",
-  "rc-tree",
-  "rc-table",
-  "rc-tabs",
-  "rc-input",
-]);
+const withTM = require('next-transpile-modules')([
+	'antd',
+	'@ant-design/icons',
+	'@ant-design/icons-svg',
+	'rc-util',
+	'rc-pagination',
+	'rc-picker',
+	'rc-menu',
+	'rc-dropdown',
+	'rc-select',
+	'rc-tree',
+	'rc-table',
+	'rc-tabs',
+	'rc-input',
+])
 
 const nextConfig = withTM({
-  reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.m?js/,
-      resolve: {
-        fullySpecified: false,
-      },
-    });
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  },
-});
+	reactStrictMode: true,
+	webpack: (config: any) => {
+		config.module.rules.push({
+			test: /\.m?js/,
+			resolve: {
+				fullySpecified: false,
+			},
+		})
+		return config
+	},
+})
 
-module.exports = nextConfig;
+module.exports = nextConfig
